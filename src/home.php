3547118b8bac821379062e6142d75a413dafa92d
JulienS-Code <julien.synaeve@gmail.com>
2024-10-22 16:59:06 +0200

    Remove ag grid links

--- src/home.php
+++ src/home.php
@@ -15,8 +15,6 @@ checkIsUser();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/home.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/ag-grid-community/dist/ag-grid-community.min.js"></script>
-
     <title>Espace Client</title>
 </head>
 <body>
