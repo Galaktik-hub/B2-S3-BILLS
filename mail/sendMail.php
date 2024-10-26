aa7f1e48a8a48810893c224a36a30a000147fed5
Galaktik-hub <dvatheking@gmail.com>
2024-10-27 00:04:34 +0200

    - Admin does not set user password when creating the account - User receives an email with instructions on what to do to set a password for his account - Creation of changePassword.php page

--- mail/sendMail.php
+++ mail/sendMail.php
@@ -18,37 +18,38 @@ $mail = new PHPMailer(true);
 //Mail adress and password
 include '../credentials/mailcredentials.php';
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = $mailsae;                     //SMTP username
-    $mail->Password   = $mdp;                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit SSL encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+function sendmail($receiverMail, $subject, $body): void {
+    global $mailsae, $mdp;
+    try {
+        //Server settings
+        $mail = new PHPMailer(true);
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;    // For debugging
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+        $mail->Username = $mailsae;                     //SMTP username
+        $mail->Password = $mdp;                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit SSL encryption
+        $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom($mailsae, 'B.I.L.L.S');
-    //$mail->addAddress('mail@example.com', 'Name');     //Add a recipient
-    //$mail->addAddress('ellen@example.com');               //Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+        //Recipients
+        $mail->setFrom($mailsae, 'B.I.L.L.S');
+        $mail->addAddress($receiverMail);     //Add a recipient
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('../image/slah-cracking.gif');    //Optional name
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('../image/slah-cracking.gif');    //Optional name
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'CA MARCHE';
-    $mail->Body    = 'J\'AI ENFIN RÉUSSI. <b>JE SUIS LE GOAT!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AltBody = "Si vous n'arrivez pas à voir ce mail, nous vous invitions à utiliser un client supportant la visualisation de courrier HTML.";
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $mail->send();
+        //echo 'Message has been sent';
+    } catch (Exception $e) {
+        //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
