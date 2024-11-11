701cca7a2773cefe9d55fb49fae47c2b2ee6ac69
KirushieldDev <kirushield@gmail.com>
2024-11-11 11:03:44 +0100

    Edit the deleteClientHome.php file, there was a double h1 and the table

--- src/deleteClientHome.php
+++ src/deleteClientHome.php
@@ -22,29 +22,27 @@
             <div class="page-content">
                 <h1> Liste des demandes de suppression de comptes </h1>
 
+                <?php
+                if(empty($clients)){
+                    echo " Il n'y a pas de demande de suppression de comptes !";
+                }
+                ?>
+
                 <div id="myGrid" class="ag-theme-quartz" style="width: 1200px; max-width: 100%;"></div>
+
+                <script>
+                    const data = <?php echo $clients_json; ?>;
+                    const columnNames = <?php echo $columns_json; ?>;
+                </script>
+                <script src="../js/constructor_agGrid_admin.js"></script>
+                <?php
+                if(isset($_GET['numClient'])){
+                    $numClient = $_GET["numClient"];
+                    include('connexion.php');
+                    echo "Le client N° $numClient a bien été supprimé.";
+                }
+                ?>
             </div>
         </div>
-
-        <h1> Liste des demandes de suppression de comptes </h1>
-        <?php
-        if(empty($clients)){
-            echo " Il n'y a pas de demande de suppression de comptes !";
-        }
-        ?>
-        <div id="myGrid" class="ag-theme-quartz" style="width: 1200px; margin: auto; max-width: 100%; font-size: 15px"></div>
-
-        <script>
-            const data = <?php echo $clients_json; ?>;
-            const columnNames = <?php echo $columns_json; ?>;
-        </script>
-        <script src="../js/constructor_agGrid_admin.js"></script>
-        <?php
-        if(isset($_GET['numClient'])){
-            $numClient = $_GET["numClient"];
-            include('connexion.php');
-            echo "Le client N° $numClient a bien été supprimé.";
-        }
-        ?>
     </body>
 </html>
