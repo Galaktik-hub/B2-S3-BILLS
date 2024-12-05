b91a491a82f6a290e5a074891b2e80d1e5e79209
Galaktik-hub <dvatheking@gmail.com>
2024-12-05 11:11:15 +0100

    - Fixed a bug when changing a password

--- src/changePassword.php
+++ src/changePassword.php
@@ -6,10 +6,10 @@ include('../include/connexion.php');
 
 if (isset($_POST['new_mdp']) && isset($_POST['pw'])) {
     $newpw = hash('sha256', $_POST['new_mdp']);
-    $req = $dbh->prepare("UPDATE `client` SET passwordClient = :newpw WHERE passwordClient = :oldpw");
-    if ($req->execute(array(':newpw' => $newpw, ':oldpw' => $_POST['pw']))) {
-        $req = $dbh->prepare("DELETE FROM `mdptemp` WHERE pw = :oldpw");
-        $req->execute(array(':oldpw' => $_POST['pw']));
+    $req = $dbh->prepare("UPDATE `client` SET passwordClient = :newpw WHERE numClient = :numClient");
+    if ($req->execute(array(':newpw' => $newpw, ':numClient' => $_SESSION['numClient']))) {
+        $req = $dbh->prepare("DELETE FROM `mdptemp` WHERE numClient = :numClient");
+        $req->execute(array(':numClient' => $_SESSION['numClient']));
         header('location: index.php');
     }
 }
@@ -24,6 +24,8 @@ $req = $dbh->prepare("SELECT * FROM client WHERE passwordClient = :pw");
 if (!($req->execute(array(':pw' => $pw)))) {
     header('location: index.php');
 }
+$line = $req->fetch();
+$_SESSION['numClient'] = $line['numClient'];
 ?>
 
 <!DOCTYPE html>
